485c853c1a603a300267c5d1af3f9ef30fe13aaa
Thinh Vu <thinhvusg@protonmail.com>
2022-07-02 13:11:47 -0500

    Version 1: designed a basic layout for the web app included header, nav, main, and footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Your GitHub's Commit History </h1>
       </header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Feature1</li>
+          <li>Feature2</li>
+          <li>About Yourself</li>
+        </ul>
+      </nav>
+      <main>
+        <h3>Your profile name is here </h3>
+        <div className='commit-detail'>
+          <p> your commit History is here</p>
+        </div>
+      </main>
+      <footer>
+        Copyright&copy; 2022 by Thinh Vu - Software Developer
+      </footer>
     </div>
   );
 }
