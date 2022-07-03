b8c946d4f2f1e6aeeafa27a9936fabbbd2955092
Thinh Vu <thinhvusg@protonmail.com>
2022-07-02 23:17:34 -0500

    Modified CSS file, added input and button on start page, and added a home page

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import bootstrap from 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Your GitHub's Commit History </h1>
-      </header>
-      <nav>
-        <ul className='nav justify-content-center'>
-          <li className='nav-item'>
-            <a href="#" className='nav-link active'>Home</a>
-          </li>
-          <li className='nav-item'>
-            <a href="#" className='nav-link'>Page 2</a>
-          </li>
-          <li className='nav-item'>
-            <a href="#" className='nav-link'>Page 3</a>
-          </li>
-          <li className='nav-item'>
-            <a href="#" className='nav-link'>About</a>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <h3>Your profile name is here </h3>
-        <div className='commit-detail'>
-          <p> your commit History is here</p>
-        </div>
-      </main>
-      <footer>
-        Copyright&copy; 2022 by Thinh Vu - Software Developer
+      <div className="App-start">
+        <h1>GitHub's Commit History</h1>
+        <div id="start-box">
+          <div className="mb-3 mt-3">
+            <label for="profile" className='form-label'>GitHub username</label>
+            <input type="text" className="form-control" id="profile" placeholder="Your Github user name" />          
+          </div>
+          <div className='mb-3 mt-3 my-font'>
+            <button type='submit' className='btn btn-dark btn-sm'>Go >>></button>
+          </div>                             
+        </div>                  
+      </div>
+      <footer className='my-font'>
+          Copyright&copy; 2022 by Thinh Vu - Software Developer
       </footer>
     </div>
   );
